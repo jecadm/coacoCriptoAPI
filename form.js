f7e36db89fa8d6e9f47401fc0beb89fbf3f16b48
rfluccas <r.f.luc.cas+github@gmail.com>
2023-05-29 07:44:21 -0300

    subo todo corregido final.

---
+++ form.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('formulario');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // Evita que el formulario se envíe automáticamente
+  const url = 'https://formspree.io/f/xrgjknok';
+  const formData = new FormData(form);
+  fetch(url, {
+    method: 'POST',
+    body: formData,
+    mode: 'no-cors'
+  })
+  .then(response => {
+    if (response.ok) {
+      console.log('Notificación enviada exitosamente');
+     
+    } else {
+      console.error('Ocurrió un error al enviar la notificación');
+    }
+  })
+  .catch(error => {
+    console.error('Ocurrió un error al enviar la notificación:', error);
+  });
+  const mensaje = document.getElementById('mensaje');
+  
+  
+   // Mostramos el mensaje de notificación
+    const notificacion = document.createElement('p');
+    notificacion.textContent = 'El formulario se ha enviado correctamente Gracias por contactarnos';
+    mensaje.appendChild(notificacion);
+}); 
+
